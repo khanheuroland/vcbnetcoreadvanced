b95c855b981bd7c066ca3c7dc567cfff59e3d592
Khanh Tran <khanh.euroland@gmail.com>
2024-09-18 10:22:32 +0700

    Add Code Session 1

---
+++ mvcdemo/Controllers/Api/DataController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvcdemo.Models;
+
+namespace mvcdemo.Controllers.Api
+{
+    public class DataController : ControllerBase
+    {
+        public List<VCBUser> GetData()
+        {
+            List<VCBUser> lstUser = new List<VCBUser>();
+            lstUser.Add(new VCBUser(){Id=1, Name="Nguyen Xuan Hung", Department="Data"});
+            lstUser.Add(new VCBUser(){Id=2, Name="Nguyen Dat", Department="IT"});
+            return lstUser;
+        }
+    }
+}
---
+++ mvcdemo/Models/VCBUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcdemo.Models
+{
+    public class VCBUser
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Department { get; set; }
+    }
+}
